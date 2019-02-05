90b1f597ce1144d8daa7114f98747449f44fdb59
Bruno Feldmann Prusch <brunoprusch@gmail.com>
2019-02-04 22:54:54 -0200

    Alterações para deploy no Heroku

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const app = require('./app'),
       mongoose = require('mongoose');
 
+app.listen(3000);
+console.log('Application started');
 
-mongoose.connect('mongodb://localhost:27017/ms-manager', { useNewUrlParser: true })
-    .then(() => {
-        app.listen(3000);
-        console.log('Application started')
-    })
-    .catch((error) => {
-        console.log('Error starting application', error);
-    })
+// mongoose.connect('mongodb://localhost:27017/ms-manager', { useNewUrlParser: true })
+//     .then(() => {
+//         app.listen(3000);
+//         console.log('Application started')
+//     })
+//     .catch((error) => {
+//         console.log('Error starting application', error);
+//     })
 
 
 module.exports = app;
